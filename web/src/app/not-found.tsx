a1c7b7b233c7ab9d222ae169d2ab36c8c9a6331e
WeissCurry <Maulanasykari@gmail.com>
2025-10-11 13:50:37 +0700

    init : ui masih komedi

---
+++ web/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+
+export default function NotFoundPage() {
+    return (
+        <div>
+            <h1>404 - Halaman Tidak Ditemukan</h1>
+            <p>Maaf, halaman yang kamu cari tidak ada.</p>
+        </div>
+    );
+}
